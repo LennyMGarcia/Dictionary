ce9104fa04c62ad6ede220fe50c353c22009886a
Lenny <Lennymgr27@gmail.com>
2022-06-04 14:57:29 -0400

    Agregar archivos de proyecto.

---
+++ Dictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //el diccionario tiene una clave y un valor
+            Dictionary<string, int> diccionario = new Dictionary<string, int>();
+
+            //rellenar el diccionario
+            diccionario.Add("juan", 5);
+
+            diccionario.Add("diandra", 48);
+
+            diccionario["maria"] = 29;
+
+            //para el foreach se necesita el keyvaluepair para ordenarlo por clave y valor
+            foreach(KeyValuePair<string, int> persona in diccionario)
+            {
+                Console.WriteLine("nombre:  " + persona.Key + " edad: " + persona.Value);
+            }
+
+
+
+            
+        }
+    }
+}
